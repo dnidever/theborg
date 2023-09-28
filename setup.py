3f117f63ac088e93cdc5057985277de57462349b
David Nidever <dnidever@lsst.org>
2023-09-28 13:34:45 -0600

    bumped version from 1.0.5 to 1.0.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='theborg',
-      version='1.0.5',
+      version='1.0.6',
       description='Artificial Neural Network routines',
       author='David Nidever, Yuan-Sen Ting',
       author_email='dnidever@montana.edu',
--- theborg/__init__.py
+++ theborg/__init__.py
@@ -1,2 +1,2 @@
 __all__ = ["model","emulator","classifier"]
-__version__ = '1.0.5'
+__version__ = '1.0.6'
